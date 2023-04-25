b8bcbf780e34558bc43e2c2f14fdb5f43b265a99
hiramekun <thescript1210@gmail.com>
2023-04-26 01:27:31 +0900

    feat: use OneFrameHttpClient and add AuthMiddleware

--- forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -4,26 +4,22 @@ package rates
 import cats.effect.Sync
 import cats.syntax.flatMap._
 import forex.programs.RatesProgram
-import forex.programs.rates.{ Protocol => RatesProgramProtocol }
-import org.http4s.HttpRoutes
+import forex.programs.rates.{Protocol => RatesProgramProtocol}
+import org.http4s.AuthedRoutes
 import org.http4s.dsl.Http4sDsl
-import org.http4s.server.Router
 
 class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
-  import Converters._, QueryParams._, Protocol._
+  import Converters._
+  import Protocol._
+  import QueryParams._
 
-  private[http] val prefixPath = "/rates"
-
-  private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
-    case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
+  private val httpRoutes: AuthedRoutes[String, F] = AuthedRoutes.of {
+    case GET -> Root / "rates" :? FromQueryParam(from) +& ToQueryParam(to) as _ =>
       rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
         Ok(rate.asGetApiResponse)
       }
   }
 
-  val routes: HttpRoutes[F] = Router(
-    prefixPath -> httpRoutes
-  )
-
+  val routes: AuthedRoutes[String, F] = httpRoutes
 }
